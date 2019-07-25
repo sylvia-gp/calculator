690f5fe349a42b1a08b9b41a2cdd7cd43adcf6c2
Sylvia Gilbert-Potts <sylvia.gilbert.potts@hotmail.com>
2019-07-25 12:23:53 +1200

    Initial clean up

--- calculator.js
+++ calculator.js
@@ -1,30 +1,16 @@
-equation = []; //running array of entries
-totallArr = [];
-total = 0; //where my equation will go
-
-temp = []; //number currently on screen
-final = ''
-
-
-window.onload = function () {
-    var e = document.getElementById('display');
-    e.innerHTML='Your equation goes here!';
-}
-
+equation = [];
+total = 0;
+temp = [];
 
 function buttonEval(num){
     var screen = document.getElementById('display')
     if (typeof num === 'number' || num === '.'){
         temp.push(num)
         screen.innerHTML = temp.join('');
-        //add number to screen and equation
-        console.log(num)
     } else if (num === 'clear'){
         screen.innerHTML = '';
         temp = [];
         equation = [];
-        //reset calculator
-        console.log('clear')
     } else if (num === '=') {
         final = temp.join('')
         equation.push(final);
@@ -46,14 +32,9 @@ function buttonEval(num){
                 screen.innerHTML = firstNum;
             }
         }
-        console.log(equation)
-        //return value of stored equation and current equation
     } else {
         equation.push(temp.join(''));
         equation.push(num);
         temp = [];
-        console.log(num)
-        console.log(equation)
-        //add num value to equation (not screen)
     }
 }
